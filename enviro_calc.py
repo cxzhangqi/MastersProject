67ae3879debcf7a96a95d7ad730acfc16a182890
Harry Horsley <hslhcam2@gmail.com>
2021-03-19 19:07:51 +0000

    Attempting flow resistivity graph

--- enviro_calc.py
+++ enviro_calc.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 
 
 def humid(temp_deg, rel_humid):
-    """ Returns the absolute humidity as a fraction and a percentage. Third return is the density of air at this humidity."""
+    """ Returns the absolute humidity as a fraction and a percentage.
+    Third return is the density of air at this humidity."""
     vapor_pressure = 1000 * 0.61121 * np.exp((18.678 - temp_deg / 234.5) * (
             temp_deg / (257.14 + temp_deg)))  # Approximate vapor_pressure with the Buck equation
 
@@ -19,7 +20,22 @@ def humid(temp_deg, rel_humid):
 
     return abs_humidity_frac, abs_humidity_percent, density_humid
 
-def sos(temp_deg, rel_humid):
+
+def sos(temp_deg, RH, pressure=np.array([1.01325e5]), ppm_conc_co2=400):
+    """Using the 1993 ISO standard it calcualtes the speed of sound in air given the molar fraction of CO_2, temperature
+     in degrees, relative humidity and pressure. Inputs can be vectors"""
+
+    # Vectorize all inputs
+    temp_deg = np.array(temp_deg, dtype=np.float64)
+    RH = np.array(RH, dtype=np.float64)
+    pressure = np.array(pressure, dtype=np.float64)
+    molar_conc_co2 = np.array(ppm_conc_co2, dtype=np.float64) / 1e6
+
+    # First calculate the molar fraction of water vapor in air based on RH
+    pass
+
+
+def sos_old(temp_deg, rel_humid):
     """ Returns the speed of sound for a given temperature and relative humidity."""
     temp_k = temp_deg + 273.15
 
@@ -66,9 +82,12 @@ def pressure_at_x_in_Pa(initial_pressure, x, temp_deg=20, freq=5e3, RH=80, press
 
     return initial_pressure * np.exp(- x * alpha * scale_factor_from_dB)
 
-def attenuation_coefficient_dBperm(freq, temp_deg, RH, pressure = np.array([1.01325e5], dtype=np.float64)):
+
+def attenuation_coefficient_dBperm(freq, temp_deg, RH, pressure=np.array([1.01325e5], dtype=np.float64)):
     """ Implements the equations to calculate the attenuation coefficient and returns this for a given
-    frequency, temperature and relative humidity."""
+    frequency, temperature and relative humidity.
+    These formulae give estimates of the absorption of pure tones to an accuracy of
+    ±10% for 0.05 < h < 5, 253 < temp (K) < 323, p0 < 200 kPa"""
 
     # Convert inputs into arrays so that we can call function on multiple inputs
     freq = np.array(freq, dtype=np.float64)
@@ -94,24 +113,14 @@ def attenuation_coefficient_dBperm(freq, temp_deg, RH, pressure = np.array([1.01
     # These equations are derived from the physical principles behind attenuation i.e. molecular relaxation and kinetics etc
     f_r0 = p_r * 24 + 4.04e4 * h * (0.02 + h) / (0.391 + h)
     f_rN = p_r * (T / T_0) ** -0.5 * (9 + 280 * h * np.exp(-4.17 * ((T / T_0) ** (-1 / 3) - 1)))
-    alpha = 8.686 * freq ** 2 * 1/p_r * (1.84e-11 * (T / T_0) ** 0.5 + (T / T_0) ** (-5 / 2) * (
+    alpha = 8.686 * freq ** 2 * 1 / p_r * (1.84e-11 * (T / T_0) ** 0.5 + (T / T_0) ** (-5 / 2) * (
             0.01275 * np.exp(-2239.1 / T) / (f_r0 + freq ** 2 / f_r0) + 0.1068 * np.exp(-3352 / T) / (
             f_rN + freq ** 2 / f_rN)))
 
     return alpha
 
 
-def attenuation_absorption_at_x(freq, temp_deg, rel_humidity, x, A_0):
-    """ Returns the dB of a signal at a distance x due to atmospheric absorption
-    for a given frequency, temperature, relative humidity and initial dB level."""
-
-    alpha = attenuation_coefficient_dBperm(freq, temp_deg, rel_humidity)
-
-    return A_0 - alpha * x
-
-
 def attenuation_coeff(freq, temp_deg, rel_humid):
-
     """Code is meant to return the attenuation coefficient using the Stokes equation, it is completely wrong."""
     # Convert frequency in Hz to radians
     w = freq * 2 * np.pi
@@ -138,7 +147,8 @@ def attenuation_coeff(freq, temp_deg, rel_humid):
 
 def vector2value(wind_vector, a, H):
     """
-
+    Returns the wind speed in each direction from a to H. Returns this as a array with each sample, a, being
+    row and columns being each microphone wind speed
     :param wind_vector: 1 x 3 array of wind vector
     :param a: n x 3 sound source location
     :param H: i x 3 microphone positions
@@ -154,27 +164,20 @@ def vector2value(wind_vector, a, H):
     return wind
 
 
-def attenuation_eq(coeff, A_0, x):
-    # A = A_0/np.power(x,2)*np.power(np.e,-coeff*x/2)
-    #
-    # A = 10*np.log10(A_0/np.power(x,2)) - coeff*np.power(x,2)/2*10*np.log10(np.e)
-    #
-    # A = A_0 + A
+def dbAttenDivergence(x, A_0): return A_0 - 20 * np.log10(x)
 
-    y = - 20 * np.log10(x) + A_0 * np.exp(-coeff * x)  # NEED TO CHECK IF THIS IS CORRECT
 
-    # x_solve = sympy.symbols('x_solve')
-    #
-    # expr = A_0 - 20*sympy.log(x_solve,10) + A_0*(1 - np.power(np.e,-coeff*x_solve/2))
-    #
-    # zero_crossing = solve(A_0 - 20*np.log10(x_solve) + A_0*(1 - np.power(np.e,-coeff*x_solve/2)),x)
+""" Attenuation functions"""
 
-    return y  # , zero_crossing[0]
 
-def dbAttenDivergence(x, A_0):
-    return A_0 - 20 * np.log10(x)
+def attenuation_absorption_at_x(freq, temp_deg, rel_humidity, x, A_0):
+    """ Returns the dB of a signal at a distance x due to atmospheric absorption
+    for a given frequency, temperature, relative humidity and initial dB level."""
 
-    # return 10*np.log10(10**(A_0/10) / x**2)
+    alpha = attenuation_coefficient_dBperm(freq, temp_deg, rel_humidity)
+
+    return A_0 - alpha * x
+
 
 def attenuation_divergence_at_r(A_0, r):
     """Calculates the attenuation due to divergence at a relative distance vector r from the source"""
@@ -183,11 +186,13 @@ def attenuation_divergence_at_r(A_0, r):
 
     return A_0 - 20 * np.log10(np.linalg.norm(r))
 
+
 def attenuation_ground_at_r(A_0, r):
     """"Calculates the attenuation due to ground effects at a relative distance vector r from the source with strength A_0 dB"""
 
     # I think I'd like to do this using data rather than equations
     pass
+
 
 def attenuation_turbulence_at_r(A_0, r):
     """Calculates the attenuation due to turbulence effects at a relative distance vector r from the source with source strength A_0 dB"""
@@ -195,11 +200,23 @@ def attenuation_turbulence_at_r(A_0, r):
     # Feel like this will be neglible, need to look into it
     pass
 
-def attenuation_vegetation_at_r(A_0, r):
-    """"Calculates the attenuation due to vegetation effects at a relative distance vector r from the source with strength A_0 dB"""
 
-    # This will also be done by data I imagine
-    pass
+def attenuation_vegetation_at_r(A_0, r, frequency, type="Forest"):
+    """"Calculates the attenuation due to vegetation effects at a relative distance vector r from the source with strength A_0 dB
+    Equations obtained from """
+
+    data_dict = {"Forest": 20} # Etc
+
+    # There are some rough approximations
+    if type == "Forest":
+        return A_0 - 0.01 * frequency**(1/3) * r
+    elif type == "Shrubs": # type must be shrubs
+        return A_0 - (0.18*np.log10(frequency) - 0.31) * r
+    elif type == None:
+        return A_0
+    else:
+        print("Invalid vegetation type given")
+
 
 def attenuation_wind_at_r(A_0, r):
     """"Calcualtes the attenuation due to wind effects at a relative distance vector r from the source with strength A_0 dB"""
@@ -208,7 +225,8 @@ def attenuation_wind_at_r(A_0, r):
 
     pass
 
-def directivity_index_dB(r, directivity_cone_angle = 45, percentage_power_in_cone = 70.):
+
+def directivity_index_dB(r, directivity_cone_angle=45, percentage_power_in_cone=70.):
     """ Calculates the directivity index for a given source pressure and pressure at an angle from the directional source
     Essentially this returns the ratio of the pressure to the pressure of a non-directional source in dB
     Here the assumption is a uniform power within the cone angle, and a different uniform power outside
@@ -225,10 +243,10 @@ def directivity_index_dB(r, directivity_cone_angle = 45, percentage_power_in_con
     solid_angle = 2 * np.pi * (1 - np.cos(directivity_cone_angle))
 
     # We can get the directivity in the cone as
-    directivity_cone = percentage_power_in_cone/100 * 4 * np.pi / solid_angle
+    directivity_cone = percentage_power_in_cone / 100 * 4 * np.pi / solid_angle
 
     # and outside the cone as
-    directivity_outside_cone = (1 - percentage_power_in_cone/100) * 4 * np.pi / (4 * np.pi - solid_angle)
+    directivity_outside_cone = (1 - percentage_power_in_cone / 100) * 4 * np.pi / (4 * np.pi - solid_angle)
 
     # Position r is within the cone angle if it is within the radius at that point
     # So we want to calculate the radius at a distance, r_x, from the tip of the cone
@@ -245,33 +263,136 @@ def directivity_index_dB(r, directivity_cone_angle = 45, percentage_power_in_con
     for i in range(r.shape[0]):
         if radial_distance[i] <= radius[i]:
             print("Within cone angle")
-            ret[i] = 10*np.log10(directivity_cone)
+            ret[i] = 10 * np.log10(directivity_cone)
         else:
             print("Outside of cone angle")
-            ret[i] = 10*np.log10(directivity_outside_cone)
+            ret[i] = 10 * np.log10(directivity_outside_cone)
     return ret
+
 
 class Signal():
 
     def __init__(self, signal_strength):
         self._signal_strength = signal_strength
 
-def SPL_at_x(coeff, x, A_0):
-    # Atmospheric pressure
-    P_a = 1.01325e5
 
+def SPL_at_x(coeff, x, A_0, P_a=1.01325e5):
     # Pressure at reference/beginning
-    P_0 = 1.01325e5 * 10 ** (A_0 / 10)
+    P_0 = P_a * 10 ** (A_0 / 10)
 
     P_x = P_0 * 1 / x ** 2 * np.exp(-coeff * x)
 
     return P_x
 
 
-# A_0 = 85
-# freq = 5000  # Hz
-# temp_deg = 20
-# RH = 85  # %
+def plot_attenuation_coefficient(tertiary_parameter, pressure=np.array([1.01325e5], dtype=np.float64),
+                                 x_axis='Temperature', y_axis='RH', tertiary='Frequency'):
+    temp_deg_values = 0
+    freq_values = 0
+    RH_values = 0
+
+    axis_limits = {'Temperature': np.arange(-10, 40, 0.2), 'Frequency': np.arange(500, 10e3, 50),
+                   'RH': np.arange(0, 100, 0.2)}
+    values = {'Temperature': temp_deg_values, 'Frequency': freq_values, 'RH': RH_values}
+
+    # Obtain X and Y in the meshgrid from our chosen axes
+    values[x_axis], values[y_axis] = np.meshgrid(axis_limits[x_axis], axis_limits[y_axis])
+
+    values[tertiary] = list(tertiary_parameter)
+
+    # Always do 2 axes, can think of a way of improving this later
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+
+    if tertiary == 'Frequency':
+        Z = attenuation_coefficient_dBperm(values["Frequency"][0], values["Temperature"], values['RH'],
+                                           pressure=pressure)
+        contours1 = ax1.contourf(values[x_axis], values[y_axis], Z)
+        fig.colorbar(contours1)
+        Z = attenuation_coefficient_dBperm(values["Frequency"][1], values["Temperature"], values['RH'],
+                                           pressure=pressure)
+        contours2 = ax2.contourf(values[x_axis], values[y_axis], Z, levels=contours1.levels)
+        fig.colorbar(contours2)
+
+
+    elif tertiary == 'Temperature':
+        Z = attenuation_coefficient_dBperm(values["Frequency"], values["Temperature"][0], values['RH'],
+                                           pressure=pressure)
+        contours1 = ax1.contourf(values[x_axis], values[y_axis], Z)
+        Z = attenuation_coefficient_dBperm(values["Frequency"], values["Temperature"][1], values['RH'],
+                                           pressure=pressure)
+        ax2.contourf(values[x_axis], values[y_axis], Z, levels=contours1.levels)
+        fig.colorbar(contours1)
+
+    elif tertiary == 'RH':
+        Z = attenuation_coefficient_dBperm(values["Frequency"], values["Temperature"], values["RH"][0],
+                                           pressure=pressure)
+        contours1 = ax1.contourf(values[x_axis], values[y_axis], Z)
+        fig.colorbar(contours1)
+        Z = attenuation_coefficient_dBperm(values["Frequency"], values["Temperature"], values["RH"][1],
+                                           pressure=pressure)
+        contours2 = ax2.contourf(values[x_axis], values[y_axis], Z, levels=contours1.levels)
+
+    ax1.set_title("{} {}".format(tertiary, values[tertiary][0]))
+
+    ax2.set_title("{} {}".format(tertiary, values[tertiary][1]))
+   # ax2.colorbar(contours2)
+    # ax1.set_xlabel("{}".format(x_axis))
+    plt.xlabel(x_axis, position=(0, 0))
+    ax1.set_ylabel(y_axis)
+    plt.show()
+
+
+def fix_parameter(parameter_to_fix='Temperature'):
+    axis_limits = {'Temperature': np.arange(-10, 40, 0.5), 'Frequency': np.arange(500, 10e3, 100),
+                   'RH': np.arange(0, 100, 0.5)}
+
+    ymax_values = []
+    ymin_values = []
+
+    if parameter_to_fix == 'Frequency':
+        X, Y = np.meshgrid(axis_limits['Temperature'], axis_limits['RH'])
+    elif parameter_to_fix == 'Temperature':
+        X, Y = np.meshgrid(axis_limits['Frequency'], axis_limits['RH'])
+    elif parameter_to_fix == 'RH':
+        X, Y = np.meshgrid(axis_limits['Temperature'], axis_limits['Frequency'])
+
+    for value in axis_limits[parameter_to_fix]:
+
+        if parameter_to_fix == 'Frequency':
+            # Obtain the attenuation coefficient values
+            attenuation_values = attenuation_coefficient_dBperm(value, X, Y)
+        elif parameter_to_fix == 'Temperature':
+            attenuation_values = attenuation_coefficient_dBperm(X, value, Y)
+        elif parameter_to_fix == 'RH':
+            attenuation_values = attenuation_coefficient_dBperm(Y, X, value)
+        else:
+            print("Incorrect paramter to fix")
+            break
+
+        max_attenuation = attenuation_values.max()
+        min_attenuation = attenuation_values.min()
+
+        ymax_values.append(max_attenuation)
+        ymin_values.append(min_attenuation)
+
+    plt.figure()
+    plt.fill_between(axis_limits[parameter_to_fix], ymin_values, ymax_values)
+
+    plt.title("Potential variation in atmospheric absorption for known {}".format(parameter_to_fix))
+    plt.xlabel(parameter_to_fix)
+    plt.ylabel("Atmospheric absorption (dB/m)")
+    plt.show()
+
+# fix_parameter("Temperature")
+# fix_parameter("Frequency")
+# fix_parameter("RH")
+#
+# plot_attenuation_coefficient([5e3, 8e3])
+# plot_attenuation_coefficient([10, 30], x_axis="Frequency", y_axis="RH", tertiary="Temperature")
+# plot_attenuation_coefficient([20, 80], x_axis="Temperature", y_axis="Frequency", tertiary="RH")
+
+
+
 # x = np.linspace(1, 80, 100)
 # y = dbAttenDivergence(x, A_0)
 # plt.figure()
@@ -292,6 +413,6 @@ def SPL_at_x(coeff, x, A_0):
 # # # y = attenuation_eq(attenuation_coeff(freq, temp_deg, rel_humidity), A_0, x)
 # # # plt.plot(x, y)
 # # #
-# plt.show()
+plt.show()
 
-#print(attenuation_coefficient_dBperm(5000,20,80))
+# print(attenuation_coefficient_dBperm(5000,20,80))
---
+++ flow_resistivity_2.py
@@ -0,0 +1,156 @@
+import numpy as np
+from scipy.special import erfc
+from enviro_calc import sos, sos_old, humid
+import matplotlib.pyplot as plt
+
+""" This file implements the equations found in Embleton 1983, Effective flow resisitivity of ground surfaces
+determined by acoustical measurements. These equations calculate the expected transmission spectra for given
+geometry of source, receiver and separation for different frequencies etc. These values, whilst not fully
+descriptive of reality, do explain a lot of the perceived spectra and as such will serve as a useful tool
+for predicting spectra. We note that the acoustic impedance of air CHANGES with temperature and humidity
+and thus this will also affect these graphs."""
+
+temp = 20
+RH = 80
+
+# Properties of air
+c_air = sos_old(temp, RH)
+_, _, density_air = humid(temp, RH)
+
+# Characteristic acoustic impedance
+Z_0 = density_air * c_air # kg/m/m/s
+
+def calculate_reflection(source_position, receiver_position):
+    """Calculates reflected distance from receiver to ground and reflection angle"""
+
+    # The horizontal distance between them
+    b = np.linalg.norm(source_position[:-1] - receiver_position[:-1])
+
+    # Ratio of z1 and z2 for similar triangles
+    ratio = source_position[-1] / receiver_position[-1]
+
+    # Second right angle triangle hypotenuse
+    hypot_2 = np.sqrt(b ** 2 / (1 + ratio) ** 2 + receiver_position[-1] ** 2)
+
+    # First hypotenuse from similar triangles
+    hypot_1 = hypot_2 * ratio
+
+    # Total distance is the sum of the two
+    r2 = hypot_1 + hypot_2
+
+    # Now we also will want the reflection angle
+    reflection_angle = np.arcsin(receiver_position[-1] / hypot_2)
+
+    return r2, reflection_angle
+
+def numerical_distance(frequency, eff_flow_resistivity, source_position, receiver_position, k1, Z1, R_p):
+    """ Returns how the distance as a multiple of wavelengths"""
+
+    r2, reflection_angle = calculate_reflection(source_position, receiver_position)
+
+    Z2 = acoustic_impedance(frequency, eff_flow_resistivity, Z1)
+
+    first_term = 0.5*(1 - 1j)
+
+    second_term = (k1 * r2)**0.5
+
+    third_term = np.cos(np.pi / 2 - reflection_angle) + Z2
+
+    return first_term * second_term * third_term
+
+def plane_wave_reflection_coefficient(frequency, eff_flow_resistivity, k1, Z1, reflection_angle):
+
+    Z2 = acoustic_impedance(frequency, eff_flow_resistivity, Z1)
+
+    k2 = wave_number(frequency, eff_flow_resistivity, k1)
+
+    numerator = Z2 * np.sin(reflection_angle) - Z1 * (1 - k1**2 / k2**2 * np.cos(reflection_angle)**2)**0.5
+
+    denominator = Z2 * np.sin(reflection_angle) + Z1 * (1 - k1**2 / k2**2 * np.cos(reflection_angle)**2)**0.5
+
+    return numerator / denominator
+
+def ground_wave_function(frequency, eff_flow_resistivity, source_position, receiver_position, k1, Z1, R_p):
+
+    w = numerical_distance(frequency, eff_flow_resistivity, source_position, receiver_position, k1, Z1, R_p)
+
+    return 1 - 1j * np.pi**0.5 * w * np.e**(-w**2) * erfc(1j * np.sqrt(w))
+
+def acoustic_impedance(frequency, eff_flow_resistivity, Z1):
+
+    R2 = Z1 * (1 + 9.08 * (frequency / eff_flow_resistivity)**-0.75)
+
+    X2 = Z1 * (11.9 * (frequency / eff_flow_resistivity)**-0.73)
+
+    return R2 + X2 * 1j
+
+def wave_number(frequency, eff_flow_resistivity, k1):
+
+    alpha = k1 * (1 + 10.8*(frequency / eff_flow_resistivity)**-0.7)
+
+    beta = k1 * (10.3 * (frequency / eff_flow_resistivity)**-0.59)
+
+    return alpha + beta*1j
+
+def pressure_at_receiver(frequency, eff_flow_resistivity, source_position, receiver_position, temp = 20, RH = 80):
+
+    _, _, density_air = humid(temp, RH)
+    c_air = sos_old(temp, RH)
+
+    # Get impedances for air (1) and ground (2)
+    Z1 = c_air * density_air
+
+    # Get distances r1 (direct) and r2 (reflected)
+    r1 = np.linalg.norm(source_position[:-1] - receiver_position[:-1])
+    r2, reflection_angle = calculate_reflection(source_position, receiver_position)
+
+    # Obtain wave numbers for sound wave in air and in the ground
+    k1 = 2*np.pi*frequency / c_air
+
+    # Obtain plane-wave reflection coefficient
+    R_p = plane_wave_reflection_coefficient(frequency, eff_flow_resistivity, k1, Z1, reflection_angle)
+
+    # Obtain the ground wave function evaluated at the numerical distance
+    F = ground_wave_function(frequency, eff_flow_resistivity, source_position, receiver_position, k1, Z1, R_p)
+
+    first_term = np.e**(1j*k1*r1) / (k1 * r1)
+
+    second_term = R_p * (np.e**(1j*k1*r2) / (k1 * r2))
+
+    third_term = (1 - R_p) * F * np.e**(1j * k1 * r2) / (k1 * r2)
+
+    return first_term + second_term + third_term
+
+# Point source of sound in 3D
+a = np.array([0, 0, 0.31], dtype=np.float64)
+
+# Arbitrary separation in x and y
+x_separation = 15.2
+y_separation = 0
+
+# Receiver position
+m = np.array([x_separation, y_separation, 1.22], dtype=np.float64)
+
+# Distance from source to receiver
+r1 = np.linalg.norm(a - m)
+
+# Distance from source to receiver via ground
+r2, reflection_angle = calculate_reflection(a, m)
+
+sigma = 100
+frequency = np.arange(100, 10e3, 1)
+y = pressure_at_receiver(frequency, sigma, a, m)
+logy = 20*np.log10(pressure_at_receiver(frequency, sigma, a, m))
+print(y.shape)
+
+plt.figure(1)
+plt.plot(frequency, np.abs(logy))
+plt.xscale("log")
+plt.show()
+
+# x = np.linspace(-4,60,100)
+# y = erfc(x)
+#
+# plt.figure(1)
+# plt.plot(x, y)
+# plt.show()
